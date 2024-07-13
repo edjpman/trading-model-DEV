c2479cc50668f57cca72d30bda63cde48b6c943c
Ethan <96897006+edjpman@users.noreply.github.com>
2024-07-13 10:51:32 -0700

    First files of the repo commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trading-model-DEV
+
+Development of datamining workflow using a particular algorithm to present to the INFO 523 class. 
---
+++ main.py
@@ -0,0 +1,71 @@
+
+###### This the the main python file to run the mining process ######
+# Additional testing will likely be completed in a Jupyter notebook testing environment 
+# Official training of the model may be completed on a cloud platform for additional compute power 
+
+
+##################
+# REQUIREMENTS
+##################
+
+# Includes python libraries and the importing of helper functions 
+# Importing of data/api calls 
+
+
+##################
+# DATA CLEANING
+##################
+
+# Adding the data to the dataframe(s), handling missing data/data errors, making conversions, etc.
+
+
+
+##################
+# DATA INTEGRATION
+##################
+
+# Merging the clean data from the various sources for into a flat format for additional preparation for mining
+
+
+
+##################
+# DATA SELECTION
+##################
+
+# Parsing out variables that are not necessary for the inital stage of the mining 
+
+
+
+##################
+# DATA TRANSFORMATION
+##################
+
+# Deriving variables if needed, normalizing variables, creating the lagging scale for the classification targets 
+
+
+
+##################
+# DATA MINING
+##################
+
+# Clustering analysis of stocks based on company attributes and market behaviors 
+# Feed-forward neural network development and training 
+
+
+
+##################
+# PATTERN EVALUATION
+##################
+
+# Outcomes of cluster analysis (technically falls before the model training)
+# Test scores of the NN on historical market data 
+# Visual check of classification score to price movement 
+
+
+
+##################
+# KNOWLEDGE REPRESENTATION
+##################
+
+# Interactive visualization of daily market behaviors to the classification score plus a running table of data 
+
