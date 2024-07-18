9daa82a2a2a89a8148522d3677537b3fc4a0aca2
Ethan <96897006+edjpman@users.noreply.github.com>
2024-07-17 22:37:54 -0700

    Requirements + Helper Function Commit

--- main.py
+++ main.py
@@ -11,6 +11,88 @@
 # Includes python libraries and the importing of helper functions 
 # Importing of data/api calls 
 
+import yfinance as yf
+import pandas as pd
+import numpy as np 
+import tensorflow as tf 
+import keras 
+from keras import layers 
+
+
+class finHelp:
+
+    def __init__(self,tkr,type,sdate,edate,intv):
+        self.tkr = tkr
+        self.type = type 
+        self.sdate = sdate
+        self.edate = edate
+        self.intv = intv
+
+    def stockdf(self,price):
+        tkr_fetch = yf.Ticker(self.tkr)
+
+        if type == 'historical_price':
+            hstprice = tkr_fetch.history(start=self.sdate,end=self.edate,interval=self.intv)
+
+            if price == 'close':
+                hstprice = hstprice['Close']
+                return hstprice
+            elif price == 'open':
+                hstprice = hstprice['Open']
+                return hstprice
+            elif price == 'high':
+                hstprice = hstprice['High']
+                return hstprice
+            elif price == 'low':
+                hstprice = hstprice['Low']
+                return hstprice
+            elif price == 'mean':
+                mnprice = (hstprice['High']+hstprice['Low'])/2
+                hstprice['Mean'] = mnprice
+                return hstprice
+            else:
+                return ValueError('Must pass "close" or "open"')
+
+        elif type == 'volume':
+            hstvol = tkr_fetch.history(start=self.sdate,end=self.edate,interval=self.intv)
+            hstvol = hstvol['Volume']
+            return hstvol
+        
+        else:
+            return ValueError('Must pass "historical_price" or "volume"')
+
+
+class deriveVar(finHelp):
+    
+    def __init__(self,tkr,type,sdate,edate,intv):
+        super().__init__(tkr,type,sdate,edate,intv)
+    
+    def rsi(self,period):
+        closeprc = self.stockdf('historical_price',start=self.sdate,end=self.edate,interval=self.intv,price='close')
+        delta = closeprc.diff()
+
+        gain = delta.where(delta > 0, 0)
+        loss = -delta.where(delta < 0, 0)
+
+        avg_gain = gain.rolling(window=period, min_periods=1).mean()
+        avg_loss = loss.rolling(window=period, min_periods=1).mean()
+
+        rels = avg_gain/avg_loss
+
+        rsi = 100 - (100/(1+rels))
+
+        rsi_df = pd.DataFrame({
+            'Close': closeprc,
+            'RSI': rsi
+        })
+
+        return rsi_df
+
+
+
+
+
+
 
 ##################
 # DATA CLEANING
@@ -48,7 +130,6 @@
 # DATA MINING
 ##################
 
-# Clustering analysis of stocks based on company attributes and market behaviors 
 # Feed-forward neural network development and training 
 
 
