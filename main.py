332acd2c7c73ea2900f820887c3823a34aefae10
Ethan <96897006+edjpman@users.noreply.github.com>
2024-08-10 09:20:12 -0700

    Updated Code

--- main.py
+++ main.py
@@ -15,22 +15,26 @@
 
 import yfinance as yf
 import pandas as pd
-import numpy as np 
-import tensorflow as tf 
-import keras 
-import os 
-from keras import layers 
+import numpy as np
+import tensorflow as tf
+import keras
+import os
+from keras import layers
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.utils import plot_model
 
 os.environ["KERAS_BACKEND"] = "tensorflow"
-
 
 class finHelp:
 
     def __init__(self,tkr,type,sdate,edate,intv):
         self.tkr = tkr
-        self.type = type 
+        self.type = type
         self.sdate = sdate
         self.edate = edate
         self.intv = intv
@@ -80,16 +84,17 @@ class finHelp:
             hstvol = tkr_fetch.history(start=self.sdate,end=self.edate,interval=self.intv)
             hstvol = hstvol['Volume']
             return hstvol
-        
+
         else:
             return ValueError('Must pass "historical_price" or "volume"')
 
 
+
 class deriveVar(finHelp):
-    
+
     def __init__(self,tkr,type,sdate,edate,intv):
         super().__init__(tkr,type,sdate,edate,intv)
-    
+
     def rsi(self,period):
         closeprc = self.stockdf(price='close')
         delta = closeprc.diff()
@@ -112,7 +117,7 @@ class deriveVar(finHelp):
         rsi_df = rsi_df.reset_index()
 
         return rsi_df
-    
+
     def targetvar(self,df,col):
         std_dev = df[col].std()
         return std_dev
@@ -134,8 +139,6 @@ class ml_preprocess:
         df.drop(columns=[time_col, 'hour'], inplace=True)
         return df
 
-
-
     def tt_split(self,target_col,time_col=None):
         df = self.df.copy(deep=True)
         features = df.drop(columns=[target_col])
@@ -147,10 +150,20 @@ class ml_preprocess:
         scaler = StandardScaler()
         X_train[numerical_cols] = scaler.fit_transform(X_train[numerical_cols])
         X_test[numerical_cols] = scaler.transform(X_test[numerical_cols])
-
         return X_train, X_test, y_train, y_test
-    
-    
+
+    def scaler(self,target_col,time_col=None):
+        df = self.df.copy(deep=True)
+        features = df.drop(columns=[target_col])
+        target = df[target_col]
+        if time_col is not None:
+            features = ml_preprocess.preprocess_time(features, time_col)
+        numerical_cols = features.select_dtypes(include=['float64', 'int64']).columns
+        scaler = StandardScaler()
+        features[numerical_cols] = scaler.fit_transform(features[numerical_cols])
+        return features, target
+
+
     def batch_ds(self,traindf,testdf,val):
         trds = traindf.batch(val)
         tsds = testdf.batch(val)
@@ -173,8 +186,8 @@ class nn_model:
         model.add(layers.Dense(self.l1_neurons, activation=self.l1_act))
         model.add(layers.Dense(self.l2_neurons, activation=self.l2_act))
         model.add(layers.Dense(4, activation=self.out_act))
-        return model 
-        
+        return model
+
     def model_compile(self):
         pass
 
@@ -188,8 +201,6 @@ class nn_model:
 
 
 
-
-
 ##################
 # DATA CLEANING
 ##################
@@ -198,7 +209,7 @@ class nn_model:
 
 ticker = 'SPY'
 start_date = '2023-01-01'
-end_date = '2024-08-02'
+end_date = '2024-02-01'
 sk = finHelp(tkr='SPY',type='historical_price',sdate=start_date,edate=end_date,intv='1h')
 dv = deriveVar(tkr='SPY', type='historical_price', sdate=start_date, edate=end_date, intv='1h')
 stk3 = sk.stockdf(price='all')
@@ -260,6 +271,38 @@ print(X_train)
 
 # Feed-forward neural network development and training 
 
+hidden_units = 10
+activation = 'relu'
+l2 = 0.01
+lr = 0.01
+epochs = 5
+batch_size = 16
+
+
+model = Sequential()
+
+model.add(layers.Dense(input_dim=len(X_train.columns),
+                       units=hidden_units,
+                       activation=activation))
+
+model.add(layers.Dense(input_dim=hidden_units,
+                       units=1,
+                       activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=Adam(learning_rate=lr),
+              metrics=['accuracy'])
+
+
+history = model.fit(X_train, y_train, epochs=15, batch_size=batch_size)
+
+train_acc = model.evaluate(X_train, y_train, batch_size=32)[1]
+test_acc = model.evaluate(X_test, y_test, batch_size=32)[1]
+print('Training accuracy: %s' % train_acc)
+print('Testing accuracy: %s' % test_acc)
+
+
+
 
 
 ##################
@@ -272,9 +315,85 @@ print(X_train)
 
 
 
+y_train_pred_proba = model.predict(X_train)
+y_train_pred = (y_train_pred_proba > 0.5).astype(int)
+
+
+y_test_pred_proba = model.predict(X_test)
+y_test_pred = (y_test_pred_proba > 0.5).astype(int)
+
+
+train_preds_df = pd.DataFrame(y_train_pred, columns=['Predicted'], index=X_train.index)
+train_preds_df['True Label'] = y_train
+
+test_preds_df = pd.DataFrame(y_test_pred, columns=['Predicted'], index=X_test.index)
+test_preds_df['True Label'] = y_test
+
+full_predictions_df = pd.concat([train_preds_df, test_preds_df])
+
+full_predictions_df = full_predictions_df.sort_index()
+
+full_dataset = sDf2.copy()
+full_dataset = full_dataset.rename_axis('Index').reset_index()
+
+merged_df = full_dataset.merge(full_predictions_df, left_on='Index', right_index=True, how='left')
+
+merged_df
+
+
+
+
+
+
+
+
 ##################
 # KNOWLEDGE REPRESENTATION
 ##################
 
 # Interactive visualization of daily market behaviors to the classification score plus a running table of data 
 
+
+ticker = 'SPY'
+start_date = '2024-05-01'
+end_date = '2024-08-01'
+sk = finHelp(tkr='SPY',type='historical_price',sdate=start_date,edate=end_date,intv='1h')
+dv = deriveVar(tkr='SPY', type='historical_price', sdate=start_date, edate=end_date, intv='1h')
+stk4 = sk.stockdf(price='all')
+
+rsi_df2 = dv.rsi(period=13).drop(columns='Close')
+
+sDf3 = stk4.merge(rsi_df,on='Datetime',how='left')
+sDf3['mov_avg'] = sDf3['Mean'].rolling(window=50).mean()
+sDf3['mov_avg_diff'] = (sDf3['mov_avg']/sDf3['Mean'])-1
+sDf3['inside_days'] = sDf3.apply(lambda x: 1 if (x['Open'] < x['prev_open']) and (x['Close'] < x['prev_close']) else 0, axis=1)
+sDf3['vol_var'] = (sDf3['Volume']/(sDf3['Volume'].median()))-1
+sDf3['abs_mnChng'] = abs(sDf3['mean_change'])
+target_level = dv.targetvar(sDf3,'mean_change')
+sDf3['target'] = sDf3['abs_mnChng'].apply(lambda x: 1 if x >= target_level else 0)
+sDf3['adjusted_target'] = sDf3['target'].shift(-10)
+sDf3['channel_ptrn'] = sDf3['abs_mnChng'].rolling(window=12).mean()
+base_future = sDf3.copy(deep=True)
+sDf3 = sDf3.drop(columns=['Open','Close','High','Low','Mean','gaps','mean_shift','mean_change','prev_open','prev_close','Datetime','date','mov_avg','abs_mnChng','target'])
+sDf3 = sDf3.iloc[50:-10]
+
+
+ml = ml_preprocess(df=sDf3)
+
+new_features, new_target = ml.scaler(target_col='adjusted_target',time_col='time')
+
+y_new_pred_proba = model.predict(new_features)
+y_new_pred = (y_new_pred_proba > 0.5).astype(int)
+
+#Consider changing the "new_features" with "base_future"
+new_preds_df = pd.DataFrame(y_new_pred, columns=['Predicted'], index=new_features.index)
+new_preds_df['True Label'] = new_target
+
+accuracy = accuracy_score(new_target, y_new_pred)
+print('Accuracy on new dataset:', accuracy)
+
+#now just add on the base_future dataset
+
+
+
+new_preds_df
